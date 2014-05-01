d7e7905c02fb5502a775fb71532e5ac593977804
Andrew Sharp <andyjsharp@users.noreply.github.com>
2014-05-01 14:55:13 +0100

    Updated to include Configuration Views

--- scripts/spaceport.sh
+++ scripts/spaceport.sh
@@ -15,12 +15,15 @@ echo " "
 grep -v '^#' servers.csv | while IFS=, read col1 col2 col3 col4 col5
 do
   echo "Server: $col1 ($col2)"
-  for f in $(find $pwd/repository/scripts -name '*.slax'); 
+  for f in $(find $pwd/repository/scripts -name '*.slax');
     do juise spaceport.slax name "$(basename "$f")" script "$f" server "$col2" user "$col3" password "$col4" deploy "$col5";
-  done; 
-  for f in $(find $pwd/repository/configlets -name '*.xml'); 
+  done;
+  for f in $(find $pwd/repository/configlets -name '*.xml');
     do juise spaceport-configlet.slax name "$(basename "$f")" configlet "$f" server "$col2" user "$col3" password "$col4";
-  done; 
+  done;
+  for f in $(find $pwd/repository/views -name '*.xml');
+    do juise spaceport-views.slax name "$(basename "$f")" view "$f" server "$col2" user "$col3" password "$col4";
+  done;
 done
 echo ""
 echo "All done."
