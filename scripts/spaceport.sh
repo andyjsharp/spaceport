7d9ac77296344fa4093e9eed2d1452ee28774d09
Andrew Sharp <andyjsharp@users.noreply.github.com>
2014-05-01 10:30:22 +0100

    Added grep to skip lines
    Now possible to skip lines in servers.csv that begin with #

--- scripts/spaceport.sh
+++ scripts/spaceport.sh
@@ -12,12 +12,15 @@ else
 fi
 echo "Processing slax scripts..."
 echo " "
-while IFS=, read col1 col2 col3 col4 col5
+grep -v '^#' servers.csv | while IFS=, read col1 col2 col3 col4 col5
 do
   echo "Server: $col1 ($col2)"
   for f in $(find $pwd/repository/scripts -name '*.slax'); 
     do juise spaceport.slax name "$(basename "$f")" script "$f" server "$col2" user "$col3" password "$col4" deploy "$col5";
   done; 
-done < servers.csv
+  for f in $(find $pwd/repository/configlets -name '*.xml'); 
+    do juise spaceport-configlet.slax name "$(basename "$f")" configlet "$f" server "$col2" user "$col3" password "$col4";
+  done; 
+done
 echo ""
 echo "All done."
